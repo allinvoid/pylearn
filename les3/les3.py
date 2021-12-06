f5562947bab23abf6b262c70e96c07e4b8923e67
allinvoid <95500989+allinvoid@users.noreply.github.com>
2021-12-06 21:32:07 +0200

    Add files via upload

---
+++ les3/les3.py
@@ -0,0 +1,3 @@
+x = ("\\");
+
+print("Escape sequences\n\n\\a\tBell (alert)\n\\b\tBackspace\n\\n\tNew line\n\\t\tHorisontal tab\n""\\"+"\\"+"\tBackslash \\ \n""\\"+"\""+"\tDouble quotation mark \"\n""\\"+"\'"+"\tSingle quotation mark \'\n")
